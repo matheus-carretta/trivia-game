68100148838065c8f699738ed62610d58cbe91f1
Mariana Mohr <marianamohr@gmail.com>
2021-06-14 16:11:52 -0300

    requisito 1 - feito

---
+++ src/reducers/user.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case '':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default user;
